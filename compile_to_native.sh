cc6861ac8786322569f17af17978901ca94c19f6
Paulo Grolli Grabin <paulograbin@gmail.com>
2024-11-05 11:41:20 -0300

    Add script to generate native image

---
+++ compile_to_native.sh
@@ -0,0 +1,5 @@
+rm -rf target
+
+mvn clean package
+
+native-image --gc=epsilon -O3 -march=native --enable-preview -cp target/downloadHTML-1.0-SNAPSHOT.jar -o download_native_experiment com.paulograbin.Main
