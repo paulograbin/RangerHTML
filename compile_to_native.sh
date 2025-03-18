09e4ec5505651cf86833569c34d57e3d29377139
Paulo Grabin <plgrabin@gmail.com>
2025-03-18 13:13:35 -0300

    Recompile program and adapt the script

--- compile_to_native.sh
+++ compile_to_native.sh
@@ -3,3 +3,5 @@ rm -rf target
 mvn clean package
 
 native-image --gc=epsilon -O3 -march=native --enable-preview -cp target/downloadHTML-1.0-SNAPSHOT.jar -o download_native_experiment com.paulograbin.Main
+
+sudo mv download_native_experiment /opt/htmlDownloader
