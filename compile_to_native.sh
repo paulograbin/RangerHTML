13445ef6f0e908e8fd6277dbdd9ffe6982fd76ac
Paulo Grabin <plgrabin@gmail.com>
2025-03-22 09:24:27 -0300

    Improve compilation script and remove unnecessary flags

--- compile_to_native.sh
+++ compile_to_native.sh
@@ -1,7 +1,5 @@
-rm -rf target
-
 mvn clean package
 
-native-image --gc=epsilon -O3 -march=native --enable-preview -cp target/downloadHTML-1.0-SNAPSHOT.jar -o download_native_experiment com.paulograbin.Main
+native-image --gc=epsilon -O3 -march=native -cp target/downloadHTML-1.0-SNAPSHOT.jar -o download_native_experiment com.paulograbin.Main
 
-sudo mv download_native_experiment /opt/htmlDownloader
+sudo cp download_native_experiment /opt/htmlDownloader
