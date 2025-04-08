f8767c010d314d1193fd668996b01ae1352dbd29
Paulo Grabin <plgrabin@gmail.com>
2025-04-08 08:58:36 -0300

    Add compilation script

---
+++ compile.sh
@@ -0,0 +1,7 @@
+sdk use java 21.0.2-graal
+
+mvn clean package
+
+mv target/downloadHTML-1.0-SNAPSHOT-jar-with-dependencies.jar target/downloadHTML-1.0-SNAPSHOT.jar
+
+mv target/downloadHTML-1.0-SNAPSHOT.jar .
