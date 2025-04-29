49e8ca43d75fe911d9d583043fdd32d7082a38bf
Paulo Grabin <plgrabin@gmail.com>
2025-04-29 10:09:50 -0300

    Update compilation script

--- compile.sh
+++ compile.sh
@@ -2,6 +2,6 @@ sdk use java 21.0.2-graal
 
 mvn clean package
 
-mv target/downloadHTML-1.0-SNAPSHOT-jar-with-dependencies.jar target/downloadHTML-1.0-SNAPSHOT.jar
+mv target/checker-2.0-jar-with-dependencies.jar target/checker-2.0.jar
 
-mv target/downloadHTML-1.0-SNAPSHOT.jar .
+mv target/checker-2.0.jar .
