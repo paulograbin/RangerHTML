8591a270f4f4893e5ff129c6334669e185a29b94
Paulo Grabin <paulograbin@gmail.com>
2025-05-06 16:22:44 -0300

    Bump version to 2.2

--- compile.sh
+++ compile.sh
@@ -2,6 +2,6 @@ sdk use java 21.0.2-graal
 
 mvn clean package
 
-mv target/checker-2.1-jar-with-dependencies.jar target/checker-2.1.jar
+mv target/checker-2.2-jar-with-dependencies.jar target/checker-2.2.jar
 
-mv target/checker-2.1.jar .
+mv target/checker-2.2.jar .
