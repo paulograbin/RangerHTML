4aea5a7cc2b64f9e041167dc0e4ac5a6283f5092
Paulo Grabin <paulograbin@gmail.com>
2025-06-29 12:55:26 -0300

    Modify compilation script

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
+#!/bin/zsh
+
 sdk use java 21.0.2-graal
 
 mvn clean package
 
 mv target/checker-2.2-jar-with-dependencies.jar target/checker-2.2.jar
-
-mv target/checker-2.2.jar .
+cp target/checker-2.2.jar .
